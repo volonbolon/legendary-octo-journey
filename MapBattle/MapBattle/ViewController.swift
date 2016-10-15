27942f44ee67f5c95585bc0d574bac88cbdcb232
Ariel Rodriguez <ariel@volonbolon.me>
2016-10-15 09:37:49 -0300

    Add random annotations

--- MapBattle/MapBattle/ViewController.swift
+++ MapBattle/MapBattle/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import Mapbox
+
+extension CLLocationCoordinate2D {
+    private mutating func randomPointOnCircle(radius:Float, center:CGPoint) {
+        let theta = Float(arc4random_uniform(UInt32.max))/Float(UInt32.max-1) * Float(M_PI) * 2.0
+        
+        let x = CGFloat(radius * cosf(theta))+center.x
+        let y = CGFloat(radius * sinf(theta))+center.y
+        self.latitude = CLLocationDegrees(x)
+        self.longitude = CLLocationDegrees(y)
+    }
+    
+    static func randomLocationInNYC() -> CLLocationCoordinate2D {
+        var l = CLLocationCoordinate2D()
+        let c = CGPoint(x: 40.760789, y: -73.980441)
+        l.randomPointOnCircle(radius: 0.02, center: c)
+        return l
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MGLMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.mapView.centerCoordinate = CLLocationCoordinate2D(latitude: 40.760789, longitude: -73.980441)
+        self.mapView.setZoomLevel(14, animated: true)
+    
+        for i in 0..<10 {
+            let title = "Annotation \(i)"
+            let p = MGLPointAnnotation()
+            p.coordinate = CLLocationCoordinate2D.randomLocationInNYC()
+            p.title = title
+            
+            self.mapView.addAnnotation(p)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +54,3 @@ class ViewController: UIViewController {
 
 
 }
-
