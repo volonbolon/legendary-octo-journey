b6423f8c383a5ad7793d6585207c138a8f197785
Ariel Rodriguez <ariel@volonbolon.me>
2016-10-15 09:54:09 -0300

    Implement evertyhing with MapView

--- MapBattle/MapBattle/ViewController.swift
+++ MapBattle/MapBattle/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+
+extension CLLocationCoordinate2D {
+    private mutating func randomPointOnCircle(radius:Float, center:CGPoint) {
+        let theta = Float(arc4random_uniform(UInt32.max))/Float(UInt32.max-1) * Float(M_PI) * 2.0
+        
+        let x = CGFloat(radius * cosf(theta))+center.x
+        let y = CGFloat(radius * sinf(theta))+center.y
+        self.latitude = CLLocationDegrees(x)
+        self.longitude = CLLocationDegrees(y)
+    }
+    
+    static func randomLocationInNYC() -> CLLocationCoordinate2D {
+        var l = CLLocationCoordinate2D()
+        let c = CGPoint(x: 40.760789, y: -73.980441)
+        l.randomPointOnCircle(radius: 0.02, center: c)
+        return l
+    }
+}
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func centerMap() {
+        let l = CLLocationCoordinate2D(latitude: 40.760789, longitude: -73.980441)
+        let region = MKCoordinateRegionMakeWithDistance(l, 16000, 16000)
+        self.mapView.setRegion(region, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.centerMap()
+        for i in 0..<10 {
+            let a = MKPointAnnotation()
+            a.coordinate = CLLocationCoordinate2D.randomLocationInNYC()
+            a.title = "Annotation \(i)"
+            self.mapView.addAnnotation(a)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
