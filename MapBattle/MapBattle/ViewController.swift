01a734b7ae74e47427ceac15c4bfb7be2a9b3623
Ariel Rodriguez <ariel@volonbolon.me>
2016-10-15 09:00:31 -0300

    Add Xcode project

---
+++ MapBattle/MapBattle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapBattle
+//
+//  Created by Ariel Rodriguez on 10/15/16.
+//  Copyright © 2016 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
