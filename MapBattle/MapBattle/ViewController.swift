ca5a1bde7b10a2eb9bbe06666bd123ce6ee9d8c5
Ariel Rodriguez <ariel@volonbolon.me>
2017-01-31 16:25:11 -0300

    Restrict map

--- MapBattle/MapBattle/ViewController.swift
+++ MapBattle/MapBattle/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 import CoreLocation
 import GoogleMaps
 
+struct Bounds {
+    let northEast:CLLocationCoordinate2D
+    let southWest:CLLocationCoordinate2D
+}
+
 extension CLLocationCoordinate2D {
     fileprivate mutating func randomPointOnCircle(_ radius:Float, center:CGPoint) {
         let theta = Float(arc4random_uniform(UInt32.max))/Float(UInt32.max-1) * Float(M_PI) * 2.0
@@ -30,10 +35,18 @@ extension CLLocationCoordinate2D {
 
 class ViewController: UIViewController {
     var mapView:GMSMapView!
+    var bounds:Bounds!
     
     override func loadView() {
+        let ne = CLLocationCoordinate2D(latitude: 40.895552, longitude: -73.780971)
+        let sw = CLLocationCoordinate2D(latitude: 40.491553, longitude: -74.276729)
+        
+        self.bounds = Bounds(northEast: ne, southWest: sw)
+        
         let camera = GMSCameraPosition.camera(withLatitude: 40.760789, longitude: -73.980441, zoom: 13.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.setMinZoom(11.0, maxZoom: 20.0)
+        mapView.delegate = self
         self.mapView = mapView
         view = mapView
     }
@@ -47,6 +60,37 @@ class ViewController: UIViewController {
             a.map = self.mapView
         }
         
+    }
+}
+
+extension ViewController: GMSMapViewDelegate { //
+    func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+        
+        var latitude  = position.target.latitude
+        var longitude = position.target.longitude
+        
+        if position.target.latitude > bounds.northEast.latitude {
+            latitude = bounds.northEast.latitude;
+        }
+        
+        if position.target.latitude < bounds.southWest.latitude {
+            latitude = bounds.southWest.latitude;
+        }
+        
+        if position.target.longitude > bounds.northEast.longitude {
+            longitude = bounds.northEast.longitude;
+        }
+        
+        if position.target.longitude < bounds.southWest.longitude {
+            longitude = bounds.southWest.longitude;
+        }
+        
+        if latitude != position.target.latitude || longitude != position.target.longitude {
+            var l = CLLocationCoordinate2D();
+            l.latitude  = latitude
+            l.longitude = longitude
+            mapView.animate(toLocation: l)
+        }
     }
 }
 
