64c7439188380d593203e91f847374515b03bc40
Ariel Rodriguez <ariel@volonbolon.me>
2017-01-31 13:31:36 -0300

    Remove cocoapods dependency

--- Podfile
+++
@@ -1,8 +0,0 @@
-platform :ios, '9.0'
-use_frameworks!
-
-target 'MapBattle'
-
-pod 'FDTake', :git => 'https://github.com/sp0n-7/FDTake.git'
-
-xcodeproj 'MapBattle/MapBattle.xcodeproj'
--- Pods/Target Support Files/FDTake/FDTake-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "FDTake.h"
-
-FOUNDATION_EXPORT double FDTakeVersionNumber;
-FOUNDATION_EXPORT const unsigned char FDTakeVersionString[];
-
